37adb0369565e72a1eda25db7030f7c509c0976c
nrjudice <Nate_Judice@stuller.com>
2019-07-05 15:45:24 -0500

    use aggregate for repository generics

--- sample-domain/npm/src/abstractions/aggregate.ts
+++ sample-domain/npm/src/abstractions/aggregate.ts
@@ -2,4 +2,4 @@ import { IEntity, EntityBase } from "./entity";
 
 export interface IAggregate<TKey=number> extends IEntity<TKey> { }
 
-export class AggregateBase extends EntityBase implements IAggregate<number> {}
+export abstract class AggregateBase extends EntityBase implements IAggregate<number> {}
--- sample-domain/npm/src/abstractions/repository.ts
+++ sample-domain/npm/src/abstractions/repository.ts
@@ -1,8 +1,10 @@
-export interface IRepository<TEntity, TKey=number> {
-    getAsync(): Promise<TEntity[]>;
-    getAsync(skip: number, take: number): Promise<TEntity[]>;
-    getAsync(id: TKey): Promise<TEntity | null>;
-    insertAsync(record: TEntity): Promise<TEntity>;
-    updateAsync(id: TKey, record: TEntity): Promise<TEntity>;
+import { IAggregate } from './aggregate';
+
+export interface IRepository<TAggregate extends IAggregate<TKey>, TKey=number> {
+    getAsync(): Promise<TAggregate[]>;
+    getAsync(skip: number, take: number): Promise<TAggregate[]>;
+    getAsync(id: TKey): Promise<TAggregate | null>;
+    insertAsync(record: TAggregate): Promise<TAggregate>;
+    updateAsync(id: TKey, record: TAggregate): Promise<TAggregate>;
     deleteAsync(id: TKey): Promise<void>;
 }

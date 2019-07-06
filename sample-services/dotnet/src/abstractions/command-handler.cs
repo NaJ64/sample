c8b5129de3187f3cf3040a441d89d5bff5894e04
NaJ64 <njudice@gmail.com>
2019-07-05 20:13:02 -0500

    add services proj to dotnet sln

--- sample-services/dotnet/src/abstractions/command-handler.cs
+++ sample-services/dotnet/src/abstractions/command-handler.cs
@@ -2,33 +2,27 @@ using System.Threading.Tasks;
 
 namespace Sample.Services.Abstractions
 {
-
-    public interface ICommandHandler
+    public interface ICommandHandler<TCommand> where TCommand : ICommand
     {
         string CommandType { get; }
-    }
-    public interface ICommandHandler<TCommand> : ICommandHandler where TCommand : ICommand
-    {
         Task HandleAsync(TCommand command);
     }
 
-    public interface ICommandHandler<TCommand, TResult> : ICommandHandler where TCommand : ICommand
+    public interface ICommandHandler<TCommand, TResult> where TCommand : ICommand
     {
+        string CommandType { get; }
         Task<TResult> HandleAsync(TCommand command);
     }
 
-    public abstract class CommandHandlerBase 
+    public abstract class CommandHandlerBase<TCommand> : ICommandHandler<TCommand> where TCommand : ICommand
     {
         public abstract string CommandType { get; }
-    }
-
-    public abstract class CommandHandlerBase<TCommand> : CommandHandlerBase, ICommandHandler<TCommand> where TCommand : ICommand
-    {
         public abstract Task HandleAsync(TCommand command);
     }
 
-    public abstract class CommandHandlerBase<TCommand, TResult> : CommandHandlerBase, ICommandHandler<TCommand, TResult> where TCommand : ICommand 
+    public abstract class CommandHandlerBase<TCommand, TResult> : ICommandHandler<TCommand, TResult> where TCommand : ICommand 
     {
+        public abstract string CommandType { get; }
         public abstract Task<TResult> HandleAsync(TCommand command);
     }
 }

3ffcef100a1fe5c8d3134498179003a17fc9d9f2
NaJ64 <njudice@gmail.com>
2019-07-22 22:32:21 -0500

    Add some placeholder nh code mapping

--- sample-infrastructure-persistence-orm/dotnet/src/nhibernate/aggregates/parent/child-schema.cs
+++ sample-infrastructure-persistence-orm/dotnet/src/nhibernate/aggregates/parent/child-schema.cs
@@ -1,3 +1,4 @@
+using NHibernate.Mapping.ByCode;
 using NHibernate.Mapping.ByCode.Conformist;
 using ParentAggregate = Sample.Domain.Aggregates.Parent;
 
@@ -5,6 +6,13 @@ namespace Sample.Infrastructure.Persistence.ORM.NHibernate.Aggregates.Parent
 {
     public class ChildSchema : ClassMapping<ParentAggregate.Child>
     { 
-
+        public ChildSchema()
+        {
+			Lazy(false);
+			Id(x => x.Id, map => map.Generator(Generators.Native));
+			Property(x => x.Id);
+            Property(x => x.ParentId);
+			Property(x => x.Description);
+        }
     }
 }
--- sample-infrastructure-persistence-orm/dotnet/src/nhibernate/aggregates/parent/parent-schema.cs
+++ sample-infrastructure-persistence-orm/dotnet/src/nhibernate/aggregates/parent/parent-schema.cs
@@ -1,3 +1,4 @@
+using NHibernate.Mapping.ByCode;
 using NHibernate.Mapping.ByCode.Conformist;
 using ParentAggregate = Sample.Domain.Aggregates.Parent;
 
@@ -5,6 +6,21 @@ namespace Sample.Infrastructure.Persistence.ORM.NHibernate.Aggregates.Parent
 {
     public class ParentSchema : ClassMapping<ParentAggregate.Parent> 
     { 
-
+        public ParentSchema()
+        {
+            Lazy(false);
+			Id(x => x.Id, map => map.Generator(Generators.Native));
+			Property(x => x.Id);
+			Property(x => x.Description);
+            Set(x => x.Children, map => 
+            {
+                map.Lazy(CollectionLazy.NoLazy);
+                map.Key(k =>
+                {
+                    k.Column(nameof(ParentAggregate.Child.ParentId));
+                    k.NotNullable(true);
+                });
+            });
+        }
     }
 }
